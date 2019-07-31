b5e3c001396aab4f788f834156ae0a83480c9a6d
Marc Wright <marc.wright@gmail.com>
2019-07-31 14:45:25 +0300

    push weather api axios

--- momentum-in-class-solution/script.js
+++ momentum-in-class-solution/script.js
@@ -3,13 +3,26 @@ console.log("loaded")
 const $backgroundImageDiv = $('.background');
 
 // GRAB RANDOM IMAGE FROM UNSPLASH
+// axios({
+//     method: 'GET',
+//     url: 'https://api.unsplash.com/photos/random/?client_id=c4268fc7a9c8c92823adc3e91336ba33b2eb873fa71c3a79ca93a12fa0d53887'
+//   })
+//   .then(response => {
+//     $backgroundImageDiv.css('background-image', `url(${response.data.urls.regular})`);
+//   })
+//   .catch(error => {
+//     console.log(error)
+//   })
+
+// GET WEATHER
 axios({
     method: 'GET',
-    url: 'https://api.unsplash.com/photos/random/?client_id=c4268fc7a9c8c92823adc3e91336ba33b2eb873fa71c3a79ca93a12fa0d53887'
+    url: 'https://api.openweathermap.org/data/2.5/weather?q=Riyadh&appid=3f09349479850459fbb04e503859d422'
   })
   .then(response => {
-    $backgroundImageDiv.css('background-image', `url(${response.data.urls.regular})`);
+    const celius = response.data.main.temp - 273.15
+    console.log(celius)
   })
   .catch(error => {
-    console.log(error)
+    console.log(error);
   })
